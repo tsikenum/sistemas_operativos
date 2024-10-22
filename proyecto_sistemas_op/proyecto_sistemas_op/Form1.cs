8564ea20e6ed63bd49cf687ca4e9cad3281080b3
tsidkenum <way405@gmail.com>
2024-10-21 20:49:08 -0600

    creacion de proyecto wf
    Creacion de proyecto windows form

---
+++ proyecto_sistemas_op/proyecto_sistemas_op/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_sistemas_op
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
