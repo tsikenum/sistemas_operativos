6c5a8b73ca77ab76bed3ea98dc5e5120a16210e6
Greivin Rodriguez <greivin.ricardo.rodriguez@uhispano.ac.cr>
2024-11-15 11:56:30 -0600

    Cambios en accesoDatos

--- proyecto_sistemas_op/proyecto_sistemas_op/Modelos/DatosPoblacion.cs
+++ proyecto_sistemas_op/proyecto_sistemas_op/Modelos/DatosPoblacion.cs
@@ -8,12 +8,16 @@ namespace proyecto_sistemas_op.Modelos
 {
     public class DatosPoblacion
     {
-        //test
+        
         public int Edad { get; set; }
         public int CantidadHombres { get; set; }
         public int CantidadMujeres { get; set; }
+        public int PrimariaIncompleta { get; set; }
         public int PrimariaCompleta { get; set; }
-        public int SecundariaIncompleta { get; set; }  //No es secundaria completa??
+        public int SecundariaCompleta { get; set; }
+        public int SecundariaIncompleta { get; set; }
         public int UniversitariaCompleta { get; set; }
+        public int UniversitariaIncompleta { get; set; }
+        public int SinEstudios { get; set; }
     }
 }
