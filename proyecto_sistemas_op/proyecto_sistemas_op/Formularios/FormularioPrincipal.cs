4edd48c55b1288a569598c1ae776cfc5598e0b49
Edwin Felipe Gonzalez Porras <efgp16@gmail.com>
2024-11-15 18:11:26 -0600

    Creacion de UI para el programa y boton de salida

--- proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.cs
+++ proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,27 @@ namespace proyecto_sistemas_op.Formularios
 {
     public partial class FormularioPrincipal : Form
     {
+        string[] fileData;
+        DataTable ageSexTable, groupTable, educationTable;
+
+        private void btnCargarArchivo_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
         public FormularioPrincipal()
         {
             InitializeComponent();
+        }
+
+        private void FormularioPrincipal_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
