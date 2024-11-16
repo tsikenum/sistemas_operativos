aa216349a7d726172330b29576a154ac8e5c5b38
Edwin Felipe Gonzalez Porras <efgp16@gmail.com>
2024-11-16 15:54:29 -0600

    TEST

--- proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.cs
+++ proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using proyecto_sistemas_op.Data;
 
 namespace proyecto_sistemas_op.Formularios
 {
@@ -16,9 +17,16 @@ namespace proyecto_sistemas_op.Formularios
         string[] fileData;
         DataTable ageSexTable, groupTable, educationTable;
 
+ 
+
         private void btnCargarArchivo_Click(object sender, EventArgs e)
         {
-        
+            AccesoDatos lector = new AccesoDatos();
+            List<Dictionary<string, string>> datos = new List<Dictionary<string, string>>();
+
+
+            datos = lector.P();
+
         }
 
         private void btnSalir_Click(object sender, EventArgs e)
