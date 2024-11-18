fd00f69658c3f792c8faaee322d07980d8a2d115
tsidkenum <way405@gmail.com>
2024-11-17 19:21:29 -0600

    Ejemplo 1 de UI funcionando

--- proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.Designer.cs
+++ proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.Designer.cs
@@ -35,61 +35,67 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.btnCargarArchivo = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dt_lista_edades_sexo = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_lista_edades_sexo)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(290, 43);
+            this.dataGridView2.Location = new System.Drawing.Point(406, 32);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 62;
             this.dataGridView2.RowTemplate.Height = 28;
-            this.dataGridView2.Size = new System.Drawing.Size(444, 491);
+            this.dataGridView2.Size = new System.Drawing.Size(335, 319);
             this.dataGridView2.TabIndex = 1;
             // 
             // dataGridView3
             // 
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(740, 43);
+            this.dataGridView3.Location = new System.Drawing.Point(768, 28);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 62;
             this.dataGridView3.RowTemplate.Height = 28;
-            this.dataGridView3.Size = new System.Drawing.Size(344, 491);
+            this.dataGridView3.Size = new System.Drawing.Size(237, 319);
             this.dataGridView3.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Location = new System.Drawing.Point(121, 5);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(218, 26);
+            this.textBox1.Size = new System.Drawing.Size(147, 20);
             this.textBox1.TabIndex = 5;
             this.textBox1.Text = "Lista de Edades Segun Sexo";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(370, 12);
+            this.textBox2.Location = new System.Drawing.Point(493, 8);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(270, 26);
+            this.textBox2.Size = new System.Drawing.Size(181, 20);
             this.textBox2.TabIndex = 6;
             this.textBox2.Text = "Listado Segun Sexo y Grupo Etareo";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(817, 12);
+            this.textBox3.Location = new System.Drawing.Point(828, 4);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(201, 26);
+            this.textBox3.Size = new System.Drawing.Size(135, 20);
             this.textBox3.TabIndex = 7;
             this.textBox3.Text = "Listado Segun Escolaridad";
             // 
             // btnCargarArchivo
             // 
-            this.btnCargarArchivo.Location = new System.Drawing.Point(834, 542);
+            this.btnCargarArchivo.Location = new System.Drawing.Point(828, 388);
+            this.btnCargarArchivo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCargarArchivo.Name = "btnCargarArchivo";
-            this.btnCargarArchivo.Size = new System.Drawing.Size(123, 42);
+            this.btnCargarArchivo.Size = new System.Drawing.Size(82, 27);
             this.btnCargarArchivo.TabIndex = 8;
             this.btnCargarArchivo.Text = "Cargar Archivo";
             this.btnCargarArchivo.UseVisualStyleBackColor = true;
@@ -97,30 +103,33 @@
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(975, 542);
+            this.btnSalir.Location = new System.Drawing.Point(936, 388);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(103, 42);
+            this.btnSalir.Size = new System.Drawing.Size(69, 27);
             this.btnSalir.TabIndex = 9;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
-            // dataGridView1
+            // dt_lista_edades_sexo
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 44);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(272, 491);
-            this.dataGridView1.TabIndex = 10;
+            this.dt_lista_edades_sexo.AccessibleName = "dt_lista_edades_sexo";
+            this.dt_lista_edades_sexo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dt_lista_edades_sexo.Location = new System.Drawing.Point(8, 29);
+            this.dt_lista_edades_sexo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dt_lista_edades_sexo.Name = "dt_lista_edades_sexo";
+            this.dt_lista_edades_sexo.RowHeadersWidth = 62;
+            this.dt_lista_edades_sexo.RowTemplate.Height = 28;
+            this.dt_lista_edades_sexo.Size = new System.Drawing.Size(384, 319);
+            this.dt_lista_edades_sexo.TabIndex = 10;
             // 
             // FormularioPrincipal
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1090, 594);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(1057, 496);
+            this.Controls.Add(this.dt_lista_edades_sexo);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnCargarArchivo);
             this.Controls.Add(this.textBox3);
@@ -128,12 +137,13 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.dataGridView3);
             this.Controls.Add(this.dataGridView2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormularioPrincipal";
             this.Text = "Procesando Hilo:";
             this.Load += new System.EventHandler(this.FormularioPrincipal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dt_lista_edades_sexo)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -147,6 +157,6 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button btnCargarArchivo;
         private System.Windows.Forms.Button btnSalir;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dt_lista_edades_sexo;
     }
 }
--- proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.cs
+++ proyecto_sistemas_op/proyecto_sistemas_op/Formularios/FormularioPrincipal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using proyecto_sistemas_op.Data;
+using proyecto_sistemas_op.Modelos;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,9 +20,40 @@ namespace proyecto_sistemas_op.Formularios
 
         private void btnCargarArchivo_Click(object sender, EventArgs e)
         {
-        
+            var accesoDatos = new AccesoDatos();
+            var datosPoblacion = accesoDatos.ParsearDatosAObjectos();
+
+            // Crear DataTable para el DataGridView principal
+            ageSexTable = new DataTable();
+            ageSexTable.Columns.Add("Edad");
+            ageSexTable.Columns.Add("Hombres");
+            ageSexTable.Columns.Add("Mujeres");
+
+            foreach (var dato in datosPoblacion)
+            {
+                ageSexTable.Rows.Add(dato.Edad, dato.CantidadHombres, dato.CantidadMujeres);
+            }
+
+            dt_lista_edades_sexo.DataSource = ageSexTable; // Enlaza con un DataGridView en el formulario
         }
 
+        private void ActualizarDataGrid(Dictionary<string, DatosEscolaridad> datos, DataGridView gridView)
+        {
+            // Crear un DataTable para los datos
+            var table = new DataTable();
+            table.Columns.Add("Grupo Etario");
+            table.Columns.Add("Cantidad");
+            table.Columns.Add("Nivel Educativo");
+
+            // Llenar el DataTable con los datos del Dictionary
+            foreach (var entry in datos)
+            {
+                table.Rows.Add(entry.Value.GrupoEtario, entry.Value.Cantidad, entry.Value.NivelEducacion);
+            }
+
+            // Enlazar el DataTable con el DataGridView
+            gridView.DataSource = table;
+        }
         private void btnSalir_Click(object sender, EventArgs e)
         {
             Application.Exit();
